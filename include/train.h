ccfda0759082cbf8b78f86e35e3866d90b99e77c
gourami32 <127053477+gourami32@users.noreply.github.com>
2023-06-04 21:27:13 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,20 +1,45 @@
-// Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TRAIN_H_
-#define INCLUDE_TRAIN_H_
+// Copyright 2021 NNTU-CS
+#include "train.h"
 
-class Train {
- private:
-  struct Cage {
-    bool light; // состояние лампочки
-    Cage *next;
-    Cage *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Cage *first; // точка входа в поезд (первый вагон)
- public:
-  Train();
-  void addCage(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
-};
-#endif  // INCLUDE_TRAIN_H_
+Train::Train() {
+  countOp = 0;
+  first = nullptr;
+}
+
+void Train::addCage(bool light) {
+  Cage* train = new Cage();
+  train->light = light;
+  if (first == nullptr) {
+    first = train;
+    first->next = train;
+    first->prev = train;
+  } else {
+    train->next = first;
+    train->prev = first->prev;
+    (first->prev)->next = train;
+    first->prev = train;
+  }
+}
+
+int Train::getLength() {
+  int length = 0;
+  first->light = true;
+  while (first->light) {
+    length = 0;
+    Cage* cur_train = first->next;
+    countOp++;
+    length++;
+    while (!cur_train->light) {
+      countOp++;
+      length++;
+      cur_train = cur_train->next;
+    }
+    cur_train->light = false;
+  }
+  countOp *= 2;
+  return length;
+}
+
+int Train::getOpCount() {
+  return countOp;
+}
